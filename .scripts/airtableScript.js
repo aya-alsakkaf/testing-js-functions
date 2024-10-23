0cc4513e0c59e4f8d6b18870c5e84b7caad3d3f9
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2024-10-23 19:10:13 +0300

    seperated

--- .scripts/airtableScript.js
+++ .scripts/airtableScript.js
@@ -5,72 +5,16 @@ const dotenv = require("dotenv").config();
 const xml2js = require("xml2js");
 
 // Constants and configuration
-// const {
-//   AIRTABLE_API_KEY,
-//   AIRTABLE_BASE_ID,
-//   AIRTABLE_TABLE_NAME,
-//   AIRTABLE_STUDENTS_TABLE_NAME,
-// } = process.env;
-const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY || argv[2];
-const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID || argv[3];
-const AIRTABLE_TABLE_NAME = process.env.AIRTABLE_TABLE_NAME || argv[4];
-const AIRTABLE_STUDENTS_TABLE_NAME =
-  process.env.AIRTABLE_STUDENTS_TABLE_NAME || argv[5];
-const STUDENT_GITHUB = process.argv[6] || "aya-alsakkaf";
+
+const STUDENT_GITHUB = argv[6] || "aya-alsakkaf";
 const STUDENT_REPO =
-  process.argv[7] || "https://github.com/aya-alsakkaf/testing-js-functions";
+  argv[7] || "https://github.com/aya-alsakkaf/testing-js-functions";
 const JEST_REPORT_PATH = "./junit.xml";
 
 // Airtable API functions
-const airtableApi = axios.create({
-  baseURL: `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}`,
-  headers: {
-    Authorization: `Bearer ${AIRTABLE_API_KEY}`,
-    "Content-Type": "application/json",
-  },
+const airtableApiServer = axios.create({
+  baseURL: `https://airtable-be.vercel.app/`,
 });
-
-async function fetchRecords(tableName) {
-  try {
-    const response = await airtableApi.get(`/${tableName}`);
-    return response.data.records;
-  } catch (error) {
-    console.error(`Error fetching records from ${tableName}:`, error.message);
-    throw error;
-  }
-}
-
-async function createOrUpdateTaskRecord(studentId, grade, notes, repo) {
-  try {
-    const existingTask = await checkStudentTask(repo);
-    if (existingTask) {
-      await updateTaskRecord(existingTask.id, grade, notes);
-    } else {
-      await createTaskRecord(studentId, grade, notes, repo);
-    }
-    console.log("Airtable updated successfully!");
-  } catch (error) {
-    console.error("Error updating Airtable:", error.message);
-    throw error;
-  }
-}
-
-async function createTaskRecord(studentId, grade, notes, repo) {
-  await airtableApi.post(`/${AIRTABLE_TABLE_NAME}`, {
-    fields: { Students: [studentId], Grade: grade, Notes: notes, Repo: repo },
-  });
-}
-
-async function updateTaskRecord(recordId, grade, notes) {
-  await airtableApi.patch(`/${AIRTABLE_TABLE_NAME}/${recordId}`, {
-    fields: { Grade: grade, Notes: notes },
-  });
-}
-
-async function checkStudentTask(repo) {
-  const tasks = await fetchRecords(AIRTABLE_TABLE_NAME);
-  return tasks.find((task) => task.fields["Repo"].includes(repo));
-}
 
 // Jest report parsing
 function parseJestReport(xmlContent) {
@@ -91,6 +35,28 @@ function parseJestReport(xmlContent) {
   });
 }
 
+const sendDataToServer = async (
+  passedTests,
+  failedTests,
+  grade,
+  studentGitHub,
+  studentRepo
+) => {
+  try {
+    await airtableApiServer
+      .post("/", {
+        passedTests,
+        failedTests,
+        grade,
+        studentGitHub,
+        studentRepo,
+      })
+      .then((response) => console.log("Sent Data Successfully"));
+  } catch (error) {
+    console.log("error in sending data to server", error);
+  }
+};
+
 // Main function
 async function main() {
   try {
@@ -103,21 +69,11 @@ async function main() {
     console.log(`Failed tests: ${failedTests}`);
     console.log(`Grade: ${grade.toFixed(2)}%`);
 
-    const students = await fetchRecords(AIRTABLE_STUDENTS_TABLE_NAME);
-    const student = students.find(
-      (s) => s.fields["GitHub Username"] === STUDENT_GITHUB
-    );
-
-    if (!student) {
-      throw new Error(
-        `Student with GitHub username ${STUDENT_GITHUB} not found`
-      );
-    }
-
-    await createOrUpdateTaskRecord(
-      student.id,
-      grade / 100,
-      `Passed Tests ${passedTests}, Failed Tests ${failedTests}`,
+    await sendDataToServer(
+      passedTests,
+      failedTests,
+      grade.toFixed(2),
+      STUDENT_GITHUB,
       STUDENT_REPO
     );
   } catch (error) {
